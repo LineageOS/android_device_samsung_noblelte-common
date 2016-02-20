0fb0f0ddf4606d0d8e15b185f4fa6fe70c88f32f
Christopher N. Hesse <raymanfx@gmail.com>
2016-02-20 01:51:31 +0100

    noble: Add renderscript variant for big cluster

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -76,6 +76,7 @@ USE_OPENGL_RENDERER := true
 
 # Renderscript
 BOARD_OVERRIDE_RS_CPU_VARIANT_32 := cortex-a53
+BOARD_OVERRIDE_RS_CPU_VARIANT_64 := cortex-a57
 
 # frameworks/native/services/surfaceflinger
 # Android keeps 2 surface buffers at all time in case the hwcomposer
