904cea382b22cf853a55556109ac2197f3ffbab6
Christopher N. Hesse <raymanfx@gmail.com>
2016-02-24 00:43:16 +0100

    noblelte: Fix device inheritance
    Change-Id: I67d01814f8fc6ef39ecf3f8b73150b6d279e2097

--- ramdisk/Android.mk
+++ ramdisk/Android.mk
@@ -1,5 +1,7 @@
 LOCAL_PATH := $(call my-dir)
 
+ifneq ($(filter nobleltetmo, $(TARGET_DEVICE)),)
+
 include $(CLEAR_VARS)
 LOCAL_MODULE		:= fstab.samsungexynos7420
 LOCAL_MODULE_TAGS	:= optional eng
@@ -63,3 +65,5 @@ LOCAL_MODULE_CLASS	:= BOOT
 LOCAL_SRC_FILES		:= ueventd.samsungexynos7420.rc
 LOCAL_MODULE_PATH	:= $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
+
+endif
