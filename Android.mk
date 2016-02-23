51d8842a42fc3307cfe86788b14a1b220dc6e160
Christopher N. Hesse <raymanfx@gmail.com>
2016-02-24 00:43:16 +0100

    noblelte: Fix device inheritance
    Change-Id: I67d01814f8fc6ef39ecf3f8b73150b6d279e2097

--- Android.mk
+++ Android.mk
@@ -16,7 +16,7 @@
 
 LOCAL_PATH := $(call my-dir)
 
-ifneq ($(filter nobletmo, $(TARGET_DEVICE)),)
+ifneq ($(filter nobleltetmo, $(TARGET_DEVICE)),)
 
 include $(call all-makefiles-under,$(LOCAL_PATH))
 
