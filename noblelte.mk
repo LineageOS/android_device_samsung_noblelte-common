97242c430e96742596857edd6ba7139d78c0dd71
Christopher N. Hesse <raymanfx@gmail.com>
2016-03-03 13:19:10 +0100

    noblelte: Update dalvik/hwui properties from stock
    Change-Id: I0477a58ca854b33a123a451f3118f2149cc882fe

--- noblelte.mk
+++ noblelte.mk
@@ -250,6 +250,8 @@ ADDITIONAL_DEFAULT_PROPERTIES += \
     ro.sys.fw.dex2oat_thread_count=4
 
 PRODUCT_PROPERTY_OVERRIDES += \
+    dalvik.vm.image-dex2oat-filter=speed \
+    dalvik.vm.dex2oat-filter=speed \
     dalvik.vm.heapstartsize=8m \
     dalvik.vm.heapgrowthlimit=256m \
     dalvik.vm.heapsize=512m \
@@ -264,15 +266,17 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.hwui.texture_cache_size=88 \
     ro.hwui.layer_cache_size=58 \
-    ro.hwui.path_cache_size=32 \
+    ro.hwui.path_cache_size=16 \
+    ro.hwui.texture_cache_flushrate=0.4 \
     ro.hwui.shape_cache_size=4 \
     ro.hwui.gradient_cache_size=2 \
-    ro.hwui.drop_shadow_cache_size=8 \
+    ro.hwui.drop_shadow_cache_size=6 \
     ro.hwui.r_buffer_cache_size=8 \
-    ro.hwui.text_small_cache_width=2048 \
-    ro.hwui.text_small_cache_height=2048 \
+    ro.hwui.text_small_cache_width=1024 \
+    ro.hwui.text_small_cache_height=1024 \
     ro.hwui.text_large_cache_width=4096 \
-    ro.hwui.text_large_cache_height=4096
+    ro.hwui.text_large_cache_height=2048 \
+    ro.hwui.fbo_cache_size=16
 
 # call Samsung LSI board support package
 $(call inherit-product, hardware/samsung_slsi-cm/exynos5/exynos5.mk)
