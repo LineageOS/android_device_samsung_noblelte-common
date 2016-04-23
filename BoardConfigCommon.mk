5461ff363ab62df5fd3baa098e890e0da162c411
Christopher N. Hesse <raymanfx@gmail.com>
2016-04-23 17:34:19 +0200

    noblelte: Enable Cortex A53 Erratum
    Change-Id: I1556c15be386e61a21e1c47ea4e8b1bb1d20b944

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -21,6 +21,9 @@ TARGET_2ND_CPU_ABI := armeabi-v7a
 TARGET_2ND_CPU_ABI2 := armeabi
 TARGET_2ND_CPU_VARIANT := cortex-a53
 
+# A53 Erratum
+TARGET_CPU_CORTEX_A53 := true
+
 # Binder
 TARGET_USES_64_BIT_BINDER := true
 
