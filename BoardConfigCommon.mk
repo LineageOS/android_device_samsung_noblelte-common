678016cab77f4e5e4beda12cc66cac7619aa0853
Christopher N. Hesse <raymanfx@gmail.com>
2016-08-01 18:38:10 +0200

    noblelte: Reenable mDNIE mode settings
     * Enable fancy CABL (content adaptive backlight )as well.
    
    Change-Id: I1b102ce9e0283ae1844afe24d6737beb2fb50409

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -110,6 +110,7 @@ TARGET_SPECIFIC_HEADER_PATH := $(LOCAL_PATH)/include
 
 # Hardware
 BOARD_HARDWARE_CLASS += device/samsung/noblelte-common/cmhw
+BOARD_HARDWARE_CLASS += hardware/samsung/cmhw
 
 # WIFI
 BOARD_WPA_SUPPLICANT_DRIVER      := NL80211
