ceeb852e7501f96bcf38601c7e5ec9e667fb3bf5
Christopher N. Hesse <raymanfx@gmail.com>
2016-09-15 21:50:45 +0200

    noblelte: Switch back to Camera2
    Change-Id: I4f9cf4eb1bf45def8e6b3f989f1754eb4410f08f

--- noblelte.mk
+++ noblelte.mk
@@ -134,10 +134,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/gps/gps.conf:system/etc/gps.conf \
     $(LOCAL_PATH)/configs/gps/gps.xml:system/etc/gps.xml
 
-# Camera
-PRODUCT_PACKAGES += \
-    Snap
-
 # Keys
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/keylayout/gpio-keys.kl:/system/usr/keylayout/gpio-keys.kl \
