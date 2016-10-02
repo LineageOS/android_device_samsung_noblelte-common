e38d7269c7f7a33193977771c386b168b31e8b9c
Christopher N. Hesse <raymanfx@gmail.com>
2016-10-02 20:12:06 +0200

    noblelte: Select H4 Bluetooth HAL
    I'm tired of CAF constantly messing up, so let's keep
    this change in the tree for now so we don't get bitten
    by this in the future again.
    
    Change-Id: I876d8c7f65943bf270b2601fc81602935f224d1d

--- system_prop.mk
+++ system_prop.mk
@@ -6,6 +6,12 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.bt.bdaddr_path="/efs/bluetooth/bt_addr"
 
+# Bluetooth workaround:
+# The new CAF code defaults to MCT HAL, but we
+# need the old H4 HAL for our Broadcom WiFi.
+PRODUCT_PROPERTY_OVERRIDES += \
+    qcom.bluetooth.soc=rome
+
 # Camera
 PRODUCT_PROPERTY_OVERRIDES += \
     camera2.portability.force_api=1
