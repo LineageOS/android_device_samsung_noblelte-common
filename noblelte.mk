e79ab0f8a0c1710c5ababebf92c7ba1aef7c4e15
Christopher N. Hesse <raymanfx@gmail.com>
2016-12-14 19:47:00 +0100

    noble: Install mixer gains config
    Change-Id: Ib2e7353852b30f87406f7671f6b10d0fda7cde38

--- noblelte.mk
+++ noblelte.mk
@@ -108,7 +108,8 @@ PRODUCT_PACKAGES += \
 # Audio
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/audio/audio_policy.conf:system/etc/audio_policy.conf \
-    $(LOCAL_PATH)/configs/audio/mixer_paths.xml:system/etc/mixer_paths.xml
+    $(LOCAL_PATH)/configs/audio/mixer_paths.xml:system/etc/mixer_paths.xml \
+    $(LOCAL_PATH)/configs/audio/mixer_gains.xml:system/etc/mixer_gains.xml
 
 PRODUCT_PACKAGES += \
     audio.a2dp.default \
