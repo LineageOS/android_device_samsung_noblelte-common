821bda4ddc418f04ce0356187c1f2de1ae5c6b07
Christopher N. Hesse <raymanfx@gmail.com>
2017-02-08 01:25:32 +0100

    noblelte: Add TWRP configs
    Change-Id: Ic022f0e96e4447fdf018e3ab6dd230a18b4ae496

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -165,7 +165,23 @@ TARGET_SCREEN_HEIGHT := 2048
 TARGET_SCREEN_WIDTH := 1536
 
 # Recovery
+#RECOVERY_VARIANT := twrp
+BOARD_HAS_DOWNLOAD_MODE := true
 TARGET_RECOVERY_FSTAB := $(LOCAL_PATH)/ramdisk/fstab.samsungexynos7420
+
+# TWRP
+ifeq ($(RECOVERY_VARIANT),twrp)
+TARGET_RECOVERY_FSTAB := $(LOCAL_PATH)/recovery/twrp.fstab
+RECOVERY_GRAPHICS_USE_LINELENGTH := true
+TW_THEME := portrait_hdpi
+TW_BRIGHTNESS_PATH := /sys/class/backlight/panel/brightness
+TW_MAX_BRIGHTNESS := 255
+TW_NO_REBOOT_BOOTLOADER := true
+TW_INCLUDE_CRYPTO := true
+TW_HAS_DOWNLOAD_MODE := true
+TW_NO_EXFAT_FUSE := true
+TW_EXCLUDE_SUPERSU := true
+endif
 
 # SELinux
 BOARD_SEPOLICY_DIRS += device/samsung/noblelte-common/sepolicy
