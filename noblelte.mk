867b1a50490ade4b3fec419b5f0878797cfa1253
isimobile <isi.mobi@gmail.com>
2017-04-23 16:35:16 +0200

    noblelte: Add missing bluetooth config files
    Change-Id: I2405f2c1f0d3d3f085e0b15fcc77b7709dfe6bb0

--- noblelte.mk
+++ noblelte.mk
@@ -65,6 +65,14 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     AdvancedDisplay
 
+#Bluetooth
+PRODUCT_COPY_FILES += \
+	$(LOCAL_PATH)/configs/bluetooth/av_performance.conf:system/etc/bluetooth/av_performance.conf \
+	$(LOCAL_PATH)/configs/bluetooth/bt_did.conf:system/etc/bluetooth/bt_did.conf \
+	$(LOCAL_PATH)/configs/bluetooth/bt_stack.conf:system/etc/bluetooth/bt_stack.conf \
+	$(LOCAL_PATH)/configs/bluetooth/iop_bt.db:system/etc/bluetooth/iop_bt.db \
+	$(LOCAL_PATH)/configs/bluetooth/iop_device_list.conf:system/etc/bluetooth/iop_device_list.conf \
+
 # Radio
 PRODUCT_PACKAGES += \
     libxml2 \
