1677448401effd2248a0307a5555fdc7d7301ac9
Christopher N. Hesse <raymanfx@gmail.com>
2017-06-18 21:48:27 +0200

    noblelte: Enable audio effects
    Change-Id: I2c715ccdb0e9d3e4edbfd0c73964919bab2c4445

--- noblelte.mk
+++ noblelte.mk
@@ -108,6 +108,7 @@ PRODUCT_PACKAGES += \
 
 # Audio
 PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/audio/audio_effects.conf:system/vendor/etc/audio_effects.conf \
     $(LOCAL_PATH)/configs/audio/audio_policy.conf:system/etc/audio_policy.conf \
     $(LOCAL_PATH)/configs/audio/mixer_paths.xml:system/etc/mixer_paths.xml \
     $(LOCAL_PATH)/configs/audio/mixer_paths_0.xml:system/etc/mixer_paths_0.xml \
